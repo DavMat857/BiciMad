6822c21c63d591ca87a87946dca7f2afb2ba7ba8
alvcamar <124206495+alvcamar@users.noreply.github.com>
2023-05-27 15:06:00 +0200

    Update conteo_rango_edades.py

--- scripts/conteo_rango_edades.py
+++ scripts/conteo_rango_edades.py
@@ -7,18 +7,28 @@ from pyspark import SparkContext, SparkConf
 import matplotlib.pyplot as plt
 from ast import literal_eval
 
+equivalencia_edad = {0: "Edad no determinada", 
+                     1: "0 - 16 años",
+                     2: "17 - 18 años",
+                     3: "19 - 26 años",
+                     4: "27 - 40 años",
+                     5: "41 - 65 años",
+                     6: " > 66 años"}
+
 def grafica_queso(claves, valores, filename):
     fig, ax = plt.subplots()
     colors = ["#1f77b4", "#ff7f0e", "#2ca02c", "#d62728", "#9467bd", "#8c564b", "#e377c2", "#7f7f7f"]
-    ax.pie(valores, labels = claves, colors = colors, startangle=90) #añadir labels = claves (al añadir, se pintan los números en la grafica)
+    ax.pie(valores, labels = claves, colors = colors, startangle=90)
     ax.set_title("GRÁFICO DE SECTORES MOSTRANDO USO POR RANGO DE EDAD.")
     ax.axis('equal') #hacemos que el gráfico sea un círculo
+    fig.savefig(filename)
     total, lst_leyenda = sum(valores), []
     for clave, valor in zip(claves, valores):
-        porcentaje = round((valor/total) * 100, 2) #cogemos el porcentaje y lo rodeamos a 1 único decimal
-        lst_leyenda.append(str(clave) + " -> " + str(porcentaje) + "%")
+        porcentaje = round((valor/total) * 100, 2) #cogemos el porcentaje y lo rodeamos a 2 decimales
+        lst_leyenda.append(str(clave) + ": " + equivalencia_edad[clave] + " con porcentaje " + str(porcentaje) + "%")
     ax.legend(lst_leyenda, loc="lower right", bbox_to_anchor=(1.13, -0.1))
     fig.savefig(filename)
+    print ("El gráfico {0} se ha guardado correctamente.".format(filename))
 
 def obtener_contenido(fila_json):
     # print(fila_json))
@@ -48,7 +58,7 @@ def main2(sc, files):
         
         rdd_contenido = rdd_contenido.union(contenido_filtrado_2_rdd) #unimos los rdd`s para tener 1 único rdd de pares
     
-    #veamos un gráfico del rango de edad para podr sacar conclusiones: 
+    #veamos un gráfico del rango de edad para poder sacar conclusiones: 
     ageRange_rdd = rdd_contenido.map(lambda par: par[0])
     ageRange_conteo_rdd = ageRange_rdd.countByValue()
     
@@ -60,26 +70,27 @@ def main2(sc, files):
     lista_claves = list(datos_ordenados.keys())
     lista_valores = list(datos_ordenados.values())
     
-    # grafica_datos_rango_edades(lista_claves, lista_valores, "gráfico_edades_VS_cantidad_viajes.jpg")
-    grafica_queso(lista_claves, lista_valores, "comparacion_rango_edades.png")
+    grafica_queso(lista_claves, lista_valores, "comparacion_rango_edades.png") #creamos una gráfica de sectores
+    
+    #eliminamos la "Edad no determinada" para poder obtener información más relevante
     claves_actualizadas = lista_claves[1:]
     valores_actualizados = lista_valores[1:]
     grafica_queso(claves_actualizadas, valores_actualizados, "comparacion_rango_edades_filtrado.png")
     
-    # calculemos ahora la media del viaje:
+    # calculemos ahora el tiempo del viaje:
     
     time_rdd = rdd_contenido.map(lambda par: par[1])
-    print(time_rdd.collect())
+    # print(time_rdd.collect())
     num_viajes = sum(lista_valores)
     suma = time_rdd.sum()
     media = round(suma / num_viajes, 3)
     print("La media de duración de los viajes (redondeada a 3 decimales) es: " + str(media) + " segundos.")
 
-if __name__ == "__main__":
+if __name__ == "__main__": #admite todas las bases de datos que le queramos introducir, no solamente 1
     if len(sys.argv) == 1:
         print("Uso: python3 {0} <filename 1.json> <filename 2.json> ... <filename n.json>".format(sys.argv[0]))
     else:
-        conf = SparkConf().setAppName("analisis base de datos -> rango edad")
+        conf = SparkConf().setAppName("analisis base de datos -> rango edad y tiempo medio")
         with SparkContext(conf=conf) as sc:
             sc.setLogLevel("ERROR")
             lst = [sys.argv[i] for i in range(1,(len(sys.argv)))] #lista con los nombres de archivos
